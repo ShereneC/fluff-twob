47a0eecb91280aff487fa5fa2fe1ef10132e7eda
ShereneC <spyhopper@cableone.net>
2021-11-07 22:17:12 -0700

    1

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-let problem1 = 'Hello World!'
+let problem1 = 'Build a Complex Object'
+let instructions1 = 'Find an item, possibly from the break room, and build an object in code. Challenge yourself to add a property of each data type, and then create a string to describe the object accessing each of those properties.'
+document.getElementById('problem1').innerHTML = problem1
+document.getElementById('instructions1').innerHTML = instructions1
 
-document.getElementById('here').innerHTML = string
+let dog = {
+  name: 'Rudy',
+  color: 'Orange',
+  size: 'Medium',
+  age: 11,
+  sex: 'Male',
+  altered: true
+}
+
+let solution1 = 'The dog, ' + dog.name + ', is a ' + dog.sex + ' with ' + dog.color + ' fur.'
+if (dog.altered) {
+  document.getElementById('solution1').innerHTML = solution1 + ' Dog is altered.'
+} else {
+  document.getElementById('solution1').innerHTML = solution1 + ' Dog is intact.'
+}
