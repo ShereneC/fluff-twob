ef6341de606bd6991f4eda4bbc5694b776b6ee9c
ShereneC <spyhopper@cableone.net>
2021-11-18 14:30:11 -0700

    21 js

--- main.js
+++ main.js
@@ -401,6 +401,19 @@ function closureLoop() {
   }
 }
 
+function whiteboard21() {
+  let problem21 = 'Best Time to Buy and Sell Stock'
+  let instructions21 = 'You are given an array prices where prices[i] is the price of a given stock on the ith day.'
+  'You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.'
+  'Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.'
+  document.getElementById('problem21').innerHTML = problem21
+  document.getElementById('instructions21').innerHTML = instructions21
+}
+
+function buyOrSell() {
+
+}
+
 whiteboard1()
 whiteboard2()
 whiteboard3()
@@ -427,3 +440,4 @@ whiteboard18()
 whiteboard19()
 whiteboard20()
 var l = closureLoop();
+whiteboard21()
