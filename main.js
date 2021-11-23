ea73bc7ccc9a5b795fef44e40f7324247811d432
ShereneC <spyhopper@cableone.net>
2021-11-23 10:32:53 -0700

    .......

--- main.js
+++ main.js
@@ -403,17 +403,15 @@ function closureLoop() {
 
 function whiteboard21() {
   let problem21 = 'Best Time to Buy and Sell Stock'
-  let instructions21 = 'You are given an array prices where prices[i] is the price of a given stock on the ith day.'
-  'You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.'
-  'Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.'
+  let instructions21 = 'You are given an array prices where prices[i] is the price of a given stock on the ith day. You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.'
   document.getElementById('problem21').innerHTML = problem21
   document.getElementById('instructions21').innerHTML = instructions21
 }
 
 
-let prices = [7, 1, 5, 3, 6, 4]
-var maxProfit = function (prices) {
-  for (let i = 0; i < length - 2; i++) {
+// let prices = [7, 1, 5, 3, 6, 4]
+function maxProfit(prices) {
+  for (let i = 0; i < prices.length - 2; i++) {
     let firstNum = prices[i]
     let secondNum = prices[i + 1]
     console.log(firstNum, secondNum)
@@ -448,4 +446,4 @@ whiteboard19()
 whiteboard20()
 var l = closureLoop();
 whiteboard21()
-console.log(maxProfit)
+maxProfit([7, 1, 5, 3, 6, 4])
